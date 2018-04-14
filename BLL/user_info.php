61e3c23f10d388e446f93927df2e8d1c3f2b6f7f
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-14 16:48:28 +0300

    commit_abdallah

--- BLL/user_info.php
+++ BLL/user_info.php
@@ -1,2 +1,15 @@
 <?php
 
+include_once '../DAL/my_db.php';
+
+class user_info extends my_db {
+
+    //here by this function i'll select the user type from the user_info 
+    //db table based on the user phone no.
+    public function user_type_select($user_phone) {
+        return $this->get_data("SELECT user_type FROM user_info "
+                        . "WHERE user_phone = ?"
+                        , "s", array(&$user_phone));
+    }
+
+}
