d06f207e51f75f1eeb7374d589c5ede64ba88cd1
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-14 17:41:40 +0300

    commit_abdallah

--- PRL/bus_location_get.php
+++ PRL/bus_location_get.php
@@ -2,9 +2,9 @@
 
 include_once '../BLL/bus.php';
 
-//i declared object from the user_info class to be used later.
-$user_info = new user_info();
+//i declared object from the bus class to be used later.
+$bus = new bus();
 
-$rs = $user_info->user_type_select($_GET['user_phone']);
+$rs = $bus->bus_location_get($_GET['driver_phone']);
 
 echo json_encode($rs->fetch_assoc());
