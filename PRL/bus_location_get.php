6efcc5a09bb4e496df497ef05d7290e4973eda79
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-14 17:34:14 +0300

    commit_abdallah

---
+++ PRL/bus_location_get.php
@@ -0,0 +1,10 @@
+<?php
+
+include_once '../BLL/user_info.php';
+
+//i declared object from the user_info class to be used later.
+$user_info = new user_info();
+
+$rs = $user_info->user_type_select($_GET['user_phone']);
+
+echo json_encode($rs->fetch_assoc());
