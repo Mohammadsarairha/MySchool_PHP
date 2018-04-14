67141276785702ced05864591c390e03e5a61f17
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-14 16:56:05 +0300

    commit_abdallah

--- DAL/my_con.php
+++ DAL/my_con.php
@@ -3,7 +3,7 @@
 class my_con {
 
     public function my_con() {
-        $con = new mysqli("localhost", "root", "", 'myschool_db');
+        $con = new mysqli("localhost", "root", "", "myschool_db");
         mysqli_query($con, "SET NAMES 'utf8'");
         mysqli_query($con, 'SET CHARACTER SET utf8');
         return $con;
