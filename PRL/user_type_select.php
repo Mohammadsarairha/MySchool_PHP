a7d44584ba7dc426cf76479593896b828f1d4d80
Mohammad <moahmmadsarairha@gmail.com>
2018-04-14 17:12:04 +0300

    sql

--- PRL/user_type_select.php
+++ PRL/user_type_select.php
@@ -8,3 +8,25 @@
 //$rs = $user_info->user_type_select($_GET['user_phone']);
 //
 //echo json_encode($rs->fetch_assoc());
+$con=new mysqli("localhost","root","","myschool_db");
+
+$st=$con->prepare("SELECT user_type FROM user_info "
+                        . "WHERE user_phone = ?");
+
+
+
+$st->bind_param("s", $_GET["user_phone"]);
+
+
+$st->execute();
+
+
+$rs=$st->get_result();
+
+
+
+$row=$rs->fetch_assoc();
+
+
+echo json_encode($row);
+
