a842844112f61c7894e40f627c08f4d2431a0fd7
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-14 17:23:30 +0300

    commit_abdallah

--- PRL/user_type_select.php
+++ PRL/user_type_select.php
@@ -1,32 +1,10 @@
 <?php
 
-//include_once '../BLL/user_info.php';
-//
-////i declared object from the user_info class to be used later.
-//$user_info = new user_info();
-//
-//$rs = $user_info->user_type_select($_GET['user_phone']);
-//
-//echo json_encode($rs->fetch_assoc());
-$con=new mysqli("localhost","root","","myschool_db");
+include_once '../BLL/user_info.php';
 
-$st=$con->prepare("SELECT user_type FROM user_info "
-                        . "WHERE user_phone = ?");
+//i declared object from the user_info class to be used later.
+$user_info = new user_info();
 
+$rs = $user_info->user_type_select($_GET['user_phone']);
 
-
-$st->bind_param("s", $_GET["user_phone"]);
-
-
-$st->execute();
-
-
-$rs=$st->get_result();
-
-
-
-$row=$rs->fetch_assoc();
-
-
-echo json_encode($row);
-
+echo json_encode($rs->fetch_assoc());
