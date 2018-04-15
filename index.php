2c0f917a82998c05ba7327111395e03b2c2bb43b
abdallah.sarayrah <abdallah.er11@gmail.com>
2018-04-15 22:28:01 +0300

    commit_abdallah

--- index.php
+++ index.php
@@ -1,13 +1,21 @@
-<!DOCTYPE html>
+﻿<!DOCTYPE html>
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 
-000webhost detales
-https://penial-allocation.000webhostapp.com/-->
+000webhost details
+-webhost: username: abdallah.er11@gmail.com
+-webhost: password: Webhost_2009
+
+-website
+https://penial-allocation.000webhostapp.com
 password: school_2017
 
+DataBase:
+DBName: id5422047_penialallocation
+username: id5422047_penialallocation1
+password: : penialallocation11
 
 -->
 <html>
